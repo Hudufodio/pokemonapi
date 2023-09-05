d52d8439ef38a13bb797c62f3f5e8499e711cdeb
Hudu <ahmedhudu21@gmail.com>
2023-09-05 19:12:13 +0200

    backflip style done

--- src/components/pokemonInfo/index.tsx
+++ src/components/pokemonInfo/index.tsx
@@ -1,30 +1,7 @@
 import './style.scss';
 
 const PokemonInfo = () => {
-	return (
-		<>
-			<h1>pikachu</h1>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
-				alt=""
-			/>
-			<div className="abilities">
-				<div className="group">
-					<h2>static</h2>
-				</div>
-				<div className="group">
-					<h2>lightning-rod</h2>
-				</div>
-			</div>
-			<div className="base-stats">
-				<h3>hp:35</h3>
-				<h3>attack:55</h3>
-				<h3>defence:40</h3>
-				<h3>special attack:50</h3>
-				<h3>speed:90</h3>
-			</div>
-		</>
-	);
+	return <></>;
 };
 
 export default PokemonInfo;
