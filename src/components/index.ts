9bcbb63af2f6aa739f6c1abc733e27cac62159fe
Hudu <ahmedhudu21@gmail.com>
2024-01-15 13:01:58 +0100

    fix page routing

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Hero } from './Hero';
+export { default as Search } from './Search';
 export { default as Card } from './Card';
 export { default as Navbar } from './Navbar';
 export { default as About } from './About';
+export { default as Home } from './Home';
