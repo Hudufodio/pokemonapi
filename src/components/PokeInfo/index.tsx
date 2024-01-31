1f72fa0a7e944deb61087adc44ac5bf42bc77864
Hudu <ahmedhudu21@gmail.com>
2024-01-31 15:31:52 +0100

    poke details

--- src/components/PokeInfo/index.tsx
+++ src/components/PokeInfo/index.tsx
@@ -1,25 +1,34 @@
 import './styles.scss';
 
-const PokeInfo = () => {
+const PokeInfo = ({ data }: any) => {
 	return (
 		<>
-			<div className="abilities">
-				<div className="group">
-					<h2>Static</h2>
-				</div>
-				<div className="group">
-					<h2>Growth</h2>
-				</div>
-			</div>
-			<hr style={{ width: '100%', marginTop: -18 }} />
-			<div className="stats">
-				<h3>hp: 35</h3>
-				<h3>attack: 55</h3>
-				<h3>defense: 40</h3>
-				<h3>special-attack: 50</h3>
-				<h3>special-defense: 50</h3>
-				<h3>speed: 90</h3>
-			</div>
+			{!data ? (
+				'No results'
+			) : (
+				<>
+					<div className="abilities">
+						{data?.abilities?.map((item: any) => {
+							return (
+								<>
+									<div className="group">
+										<h2>{item.name}</h2>
+									</div>
+								</>
+							);
+						})}
+					</div>
+					<hr style={{ width: '100%', marginTop: -18 }} />
+					<div className="stats">
+						<h3>hp: 35</h3>
+						<h3>attack: 55</h3>
+						<h3>defense: 40</h3>
+						<h3>special-attack: 50</h3>
+						<h3>special-defense: 50</h3>
+						<h3>speed: 90</h3>
+					</div>
+				</>
+			)}
 		</>
 	);
 };
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -25,7 +25,7 @@ const Card = ({ data, loading }: any) => {
 										<h1>{item.name}</h1>
 									</div>
 									<div className="cardBack" onMouseLeave={() => setIsFlipped('')}>
-										<PokeInfo />
+										<PokeInfo data={data} />
 									</div>
 								</ReactCardFlip>
 							</div>
